c396f1222d22b3e2f6dddeedacc7c81cec7ae35e
ivanov6444 <a.ivanov6444@gmail.com>
2022-04-25 14:18:38 +0300

    Отображение списка героев

---
+++ app-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+   { id: 11, name: 'Dr Nice', superpower: 'hypnosis'},
+   { id: 12, name: 'Narco', superpower: 'dope'},
+   { id: 13, name: 'Bombasto', superpower: 'BOMBS'},
+   { id: 14, name: 'Celeritas', superpower: 'speed'},
+   { id: 15, name: 'Magneto', superpower: 'mutant'},
+   { id: 16, name: 'RubberMan', superpower: 'elasticity'},
+   { id: 17, name: 'Dynama', superpower: 'electro'},
+   { id: 18, name: 'Dr IQ', superpower: 'intelligence'},
+   { id: 19, name: 'Magma', superpower: 'lava'},
+   { id: 20, name: 'Tornado', superpower: 'wind'},
+
+];
